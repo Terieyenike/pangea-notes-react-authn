223c1764460187b90a293d6a9ee3e76d54d97203
Oteri Eyenike <teyenike1@gmail.com>
2023-11-12 16:34:13 +0100

    Feat: appwrite databases function - create and list documents

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,26 @@
+import { Client, Databases, ID, Query } from 'appwrite';
+
+const client = new Client();
+const databases = new Databases(client);
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('6550de836c81d837f065');
+
+export const create = (data:any) =>
+  databases.createDocument(
+    "6550e0840533d448907f",
+    "6550e0a836e0927b472f",
+    ID.unique(),
+    data
+  );
+
+export const getList = databases.listDocuments(
+  "6550e0840533d448907f",
+  "6550e0a836e0927b472f",
+  [Query.orderDesc("$createdAt")]
+);
+
 export const getDateTime = (dateString: string) => {
   const date = new Date(dateString);
   if (date.toString() === "Invalid Date") {
