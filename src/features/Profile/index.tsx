a5c0b62874a6e2b423e624ab66096742ca074630
Oteri Eyenike <teyenike1@gmail.com>
2023-11-15 09:49:11 +0100

    Fix: remove user phone number

--- src/features/Profile/index.tsx
+++ src/features/Profile/index.tsx
@@ -9,7 +9,6 @@ const Profile = () => {
       <div>Email: {user?.email}</div>
       <div>First Name: {user?.profile?.first_name}</div>
       <div>Last Name: {user?.profile?.last_name}</div>
-      {user.profile.phone && <div>Phone: {user?.profile?.phone}</div>}
       <div>Logged from {user?.profile["Last-Login-City"]}</div>
       <div>Token: {user?.active_token?.token}</div>
     </div>
